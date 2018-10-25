75a8446f94afc5388ca56db18f9e7df518f28d59
arpablo <armin.pfarr@gmail.com>
2018-10-25 13:31:01 +0200

    Initial project stup

---
+++ src/main/java/de/arpablo/frida/FridaApplication.java
@@ -0,0 +1,12 @@
+package de.arpablo.frida;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FridaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FridaApplication.class, args);
+	}
+}
---
+++ src/test/java/de/arpablo/frida/FridaApplicationTests.java
@@ -0,0 +1,16 @@
+package de.arpablo.frida;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FridaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
